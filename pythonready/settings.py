ef7d9c56243b930ea90078eb5a0273e980d6d081
Hayden Eastwood <beastflow@gmail.com>
2020-08-02 11:13:22 +0100

    blah blah:

--- pythonready/settings.py
+++ pythonready/settings.py
@@ -212,5 +212,25 @@ TINYMCE_DEFAULT_CONFIG = {
         ,'classes': 'output'
     }
     ]
-
-  }
+}
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+            }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
